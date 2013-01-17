e4a3dce354485886f9942601304ba90a1867d2ba
Jason Chu <chuchuuu@stanford.edu>
2013-01-17 15:24:29 -0800

    Slightly off center canvases. Logic almost ready for multiple shapes.

--- js/shadowboxing.js
+++ js/shadowboxing.js
@@ -42,8 +42,19 @@ var sandCanvas = null;
 var sandContext = null;
 var sandData = null;
 var shapeCanvas = null;
+var shapeContext = null;
 var shapeData = null;
-var shapeData = null;
+
+var masterCanvas = null;
+var masterCanvasContext = null;
+var masterCanvasData = null;
+
+var canvas0 = null;
+var canvas0Context = null;
+var canvas0Data = null;
+
+var shapes = [];
+var shapeContexts = [];
 
 var started = false;
 
@@ -97,22 +108,21 @@ function initializeDOMElements() {
     shadowCanvas.style.display = SHOW_SHADOW ? 'block' : 'none';
     document.getElementById('capture').appendChild(shadowCanvas);
     shadowContext = shadowCanvas.getContext('2d');
-    
-    sandCanvas = document.createElement('canvas');
-    sandCanvas.setAttribute('id', 'sandCanvas');
-    sandCanvas.setAttribute('width', mWidth);
-    sandCanvas.setAttribute('height', mHeight);
-    sandCanvas.style.display = SHOW_SHADOW ? 'block' : 'none';
-    document.getElementById('sandbox').appendChild(sandCanvas);
-    sandContext = sandCanvas.getContext('2d');
-    
-    shapeCanvas = document.getElementById('shapeCanvas');
-    shapeCanvas.setAttribute('width', mWidth);
-    shapeCanvas.setAttribute('height', mHeight);
-    shapeCanvas.style.display = SHOW_SHADOW ? 'block' : 'none';
-    document.getElementById('sandbox').appendChild(shapeCanvas);
-    shapeContext = shapeCanvas.getContext('2d');
+        
+//    shapeCanvas = document.getElementById('shapeCanvas');
+//    shapeCanvas.setAttribute('width', mWidth);
+//    shapeCanvas.setAttribute('height', mHeight);
+//    shapeCanvas.style.display = SHOW_SHADOW ? 'block' : 'none';
+//    document.getElementById('sandbox').appendChild(shapeCanvas);
+//    shapeContext = shapeCanvas.getContext('2d');
 
+	canvas0 = document.getElementById('canvas0');
+    canvas0.setAttribute('width', mWidth);
+    canvas0.setAttribute('height', mHeight);
+    canvas0.style.display = SHOW_SHADOW ? 'block' : 'none';
+    document.getElementById('canvasesdiv').appendChild(canvas0);
+    canvas0Context = canvas0.getContext('2d');
+ 
 	// var test = shapeContext.getImageData(0, 0, shapeCanvas.width, shapeCanvas.height);
 	// console.log("test");
 	// console.log(test.data[0]);
@@ -120,18 +130,34 @@ function initializeDOMElements() {
 	// console.log(test.data[2]);
 	// console.log(test.data[3]);
     //draw a square for now
-    var imgData=shapeContext.createImageData(100,100);
-	for (var i=0;i<imgData.data.length;i+=4)
-	  {
-	  imgData.data[i+0]=0;
-	  imgData.data[i+1]=0;
-	  imgData.data[i+2]=0;
+
+//    var imgData=shapeContext.createImageData(100,100);
+//	for (var i=0;i<imgData.data.length;i+=4)
+//	  {
+//	  imgData.data[i+0]=0;
+//	  imgData.data[i+1]=0;
+//	  imgData.data[i+2]=0;
+//	
+//		//this is where a problem is
+//	  imgData.data[i+3]=255;
+//	  }
+//	shapeContext.putImageData(imgData,0,0);
+//	shapeData = shapeContext.getImageData(0, 0, mWidth, mHeight);
+//	
+
+	canvas0Data = canvas0Context.createImageData(40, 40);
+	for (var j = 0; j < canvas0Data.data.length; j += 4) {
+		canvas0Data.data[j+0] = 0;
+		canvas0Data.data[j+1] = 0;
+		canvas0Data.data[j+2] = 0;
+		canvas0Data.data[j+3] = 255;
+	}
+	canvas0Context.putImageData(canvas0Data, 0, 0);
+	canvas0Data = canvas0Context.getImageData(0, 0, mWidth, mHeight);
+	shapes.push(canvas0Data);
+	shapeContexts.push(canvas0Context);
+
 	
-		//this is where a problem is
-	  imgData.data[i+3]=255;
-	  }
-	shapeContext.putImageData(imgData,10,10);
-	shapeData = shapeContext.getImageData(0, 0, shapeCanvas.width, shapeCanvas.height);
 }
 
 /*
@@ -250,12 +276,12 @@ function hasOverlapTest(pixelData1, pixelData2) {
 	//shapeContext.putImageData(pixelData2, 0, 0);
 }
 
-function turnShapeGreen(shpData) {
+function turnShapeGreen(shpData, shpContext) {
 	for (var i = 0; i < shpData.data.length; i += 4) {
 		var shapeR = (shpData.data[i+0] == 0);
 		var shapeG = (shpData.data[i+1] == 0);
 		var shapeB = (shpData.data[i+2] == 0);
-		var shapeA = (shapeData.data[i+3] == 255);
+		var shapeA = (shpData.data[i+3] == 255);
 		
 		var shapePixelIsBlack = (shapeR && shapeG && shapeB && shapeA);
 		if (shapePixelIsBlack) {
@@ -265,15 +291,15 @@ function turnShapeGreen(shpData) {
 		  	shpData.data[i+3]=255;
 		}
 	}
-	shapeContext.putImageData(shpData,10,10);
+	shpContext.putImageData(shpData,0,0);
 }
 
-function turnShapeBlack(shpData) {
+function turnShapeBlack(shpData, shpContext) {
 	for (var i = 0; i < shpData.data.length; i += 4) {
 		var shapeR = (shpData.data[i+0] == 0);
 		var shapeG = (shpData.data[i+1] == 255);
 		var shapeB = (shpData.data[i+2] == 0);
-		var shapeA = (shapeData.data[i+3] == 255);
+		var shapeA = (shpData.data[i+3] == 255);
 		
 		var shapePixelIsGreen = (shapeR && shapeG && shapeB && shapeA);
 		if (shapePixelIsGreen) {
@@ -283,81 +309,81 @@ function turnShapeBlack(shpData) {
 		  	shpData.data[i+3]=255;
 		}
 	}
-	shapeContext.putImageData(shpData,10,10);
+	shpContext.putImageData(shpData,0,0);
 }
 
-function compare() {
-	
-	//Currently, renderShadow does all of the following code. So, we don't need to press the
-	//compare button anymore. This is just a test though.
-	
-	sandContext.putImageData(pixelData, 0, 0);
-	sandData = sandContext.getImageData(0, 0, sandCanvas.width, sandCanvas.height);
-
-	if (hasOverlapTest(sandData, shapeData)) {
-		//console.log("turn shape green func");
-		turnShapeGreen(shapeData);
-	} else {
-		//console.log("turn back");
-		turnShapeBlack(shapeData);
-	}
-	
-	setTimeout(compare, 0);
-	
-	//sandContext.putImageData(shapeData, 0, 0);
-	
-	//need to compare the sandData to the shapeData
-	
-	
-	/* LAO'S SIMON SAYS COMPARE
-	console.log("pixel data data");
-	var match = true;
-	
-	for (var i = 0; i < sandData.data.length; i = i + 4) {
-		//console.log(sandData.data[i]);
-		//console.log(sandData.data[i+1]);
-		//console.log(sandData.data[i+2]);
-		var r0 = sandData.data[i] == 0;
-		var g0 = sandData.data[i+1] == 0;
-		var b0 = sandData.data[i+2] == 0;
-		var isShadow = (r0 && g0 && b0);
-		
-		if (!isShadow) {
-		
-			var r255 = doData.data[i] == 255;
-			//if (r) console.log("r match");
-			// if (!r) {
-			// 	match = false;
-			// }
-			var g255 = doData.data[i+1] == 255;
-			//if (g) console.log("r match");
-			// if (!g) {
-			// 	match = false;
-			// }
-			var b255 = doData.data[i+2] == 255;
-			//if (b) console.log("r match");
-			// if (!b) {
-			// 	match = false;
-			// }
-			
-			var isWhite = (r255 && g255 && b255);
-		
-			if (!isWhite) {
-				match = false;
-				doData.data[i] = 255;
-				doData.data[i+1] = 0;
-				doData.data[i+2] = 0;
-			}
-		}
-	}
-	if (match) {
-		console.log("match!");
-	} else {
-		console.log("no match");
-		doContext.putImageData(doData, 0, 0);
-	}
-	*/
-}
+//function compare() {
+//	
+//	//Currently, renderShadow does all of the following code. So, we don't need to press the
+//	//compare button anymore. This is just a test though.
+//	
+//	sandContext.putImageData(pixelData, 0, 0);
+//	sandData = sandContext.getImageData(0, 0, sandCanvas.width, sandCanvas.height);
+//
+//	if (hasOverlapTest(sandData, shapeData)) {
+//		//console.log("turn shape green func");
+//		turnShapeGreen(shapeData);
+//	} else {
+//		//console.log("turn back");
+//		turnShapeBlack(shapeData);
+//	}
+//	
+//	setTimeout(compare, 0);
+//	
+//	//sandContext.putImageData(shapeData, 0, 0);
+//	
+//	//need to compare the sandData to the shapeData
+//	
+//	
+//	/* LAO'S SIMON SAYS COMPARE
+//	console.log("pixel data data");
+//	var match = true;
+//	
+//	for (var i = 0; i < sandData.data.length; i = i + 4) {
+//		//console.log(sandData.data[i]);
+//		//console.log(sandData.data[i+1]);
+//		//console.log(sandData.data[i+2]);
+//		var r0 = sandData.data[i] == 0;
+//		var g0 = sandData.data[i+1] == 0;
+//		var b0 = sandData.data[i+2] == 0;
+//		var isShadow = (r0 && g0 && b0);
+//		
+//		if (!isShadow) {
+//		
+//			var r255 = doData.data[i] == 255;
+//			//if (r) console.log("r match");
+//			// if (!r) {
+//			// 	match = false;
+//			// }
+//			var g255 = doData.data[i+1] == 255;
+//			//if (g) console.log("r match");
+//			// if (!g) {
+//			// 	match = false;
+//			// }
+//			var b255 = doData.data[i+2] == 255;
+//			//if (b) console.log("r match");
+//			// if (!b) {
+//			// 	match = false;
+//			// }
+//			
+//			var isWhite = (r255 && g255 && b255);
+//		
+//			if (!isWhite) {
+//				match = false;
+//				doData.data[i] = 255;
+//				doData.data[i+1] = 0;
+//				doData.data[i+2] = 0;
+//			}
+//		}
+//	}
+//	if (match) {
+//		console.log("match!");
+//	} else {
+//		console.log("no match");
+//		doContext.putImageData(doData, 0, 0);
+//	}
+//	*/
+//}
 
 /*
  * In a loop: gets the current frame of video, thresholds it to the background frames,
@@ -379,12 +405,13 @@ function renderShadow() {
 	//sandData = sandContext.getImageData(0, 0, sandCanvas.width, sandCanvas.height);
 
 	//if (hasOverlapTest(sandData, shapeData)) {
-	if (hasOverlapTest(pixelData, shapeData)) {
+		console.log(shapes[0]);
+	if (hasOverlapTest(pixelData, shapes[0])) {
 		//console.log("turn shape green func");
-		turnShapeGreen(shapeData);
+		turnShapeGreen(shapes[0], shapeContexts[0]);
 	} else {
 		//console.log("turn back");
-		turnShapeBlack(shapeData);
+		turnShapeBlack(shapes[0], shapeContexts[0]);
 	}
 	//End of test code.
 
